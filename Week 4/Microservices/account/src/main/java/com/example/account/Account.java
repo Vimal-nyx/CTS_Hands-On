dbb1c77701e5b9b0b2661bc1dc8f649addbd79a7
Vimal-nyx <vimaladhittantamizhazhahan@gmail.com>
2026-07-19 19:34:42 +0530

    added week 4

---
+++ Week 4/Microservices/account/src/main/java/com/example/account/Account.java
@@ -0,0 +1,3 @@
+package com.example.account;
+
+public record Account(String number, String type, double balance) {}
---
+++ Week 4/Microservices/loan/src/main/java/com/example/loan/Loan.java
@@ -0,0 +1,3 @@
+package com.example.loan;
+
+public record Loan(String number, String type, double loan, double emi, int tenure) {}
